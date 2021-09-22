8b5b630728cbbfc1a4e448ca5111286da2fbeb1d
ajay <ajaydev2223@gmail.com>
2021-09-22 13:25:50 +0530

    inital commit

---
+++ jsworks/script.js
@@ -0,0 +1,12 @@
+//-comment
+//console.log-javascript print
+//identifiers-simply name is called identifier
+//variable name-lowercase,noun,_
+//functionname-if two words...first letter of the second word must be capital
+//print_person() -snake Notification-python
+//printPerson() -camel case -javascript
+//classname first letter uppercase and second word first letter also cap
+//keywords for variable-var let const
+const age=21;
+var name="AJAY";
+console.log("hai i'm"+name+age+"years old")
